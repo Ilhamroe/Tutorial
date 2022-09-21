f5b144ac81fa5283d1b6e8123957920a2ad229ed
Ilhamroe <111882022+Ilhamroe@users.noreply.github.com>
2022-09-21 20:06:06 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tTugas Pendahuluan #2\n");
+
+    printf("\nNo.1\n");
+    printf("Nama\t;%s\n","Mohammad Ilham Ramadani");
+    printf("Kelas\t;%s\n","D3 IT A");
+    printf("NRP\t;%s\n","3*******21\n");
+
+    printf("\nNo.2\n");
+    int a=1024,b=4096;
+    printf("Hasil Penjumlahan:%d\n",a+b);
+
+    printf("\nNo.3\n");
+    int m;
+    int n;
+    scanf("%d",&m);
+    scanf("%d",&n);
+    printf("Hasilnya:%d\n",m+n);
+
+    printf("\nNo.4\n");
+    float x;
+    int y=50;
+    scanf("%f",&x);
+    printf("Hasil:%f\n",x*y);
+
+
+    printf("\nNo.5\n");
+    printf("Bilangan float:%f\n",3.21);
+    printf("21.3\n");
+
+    printf("\nNo.6\n");
+    printf("char:%c\n",'o');
+
+    return 0;
+}
